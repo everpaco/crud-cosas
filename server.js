5e5a4a57e6c5cf75a32f41870cdb011da39928ab
everpaco <everpaco200@gmail.com>
2026-05-04 16:15:39 -0400

    chore: run sequelize migrations before server start (programmatic)

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 'use strict';
 require('dotenv').config();
+const { exec } = require('child_process');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+function runMigrations() {
+  return new Promise((resolve, reject) => {
+    console.log('Running migrations...');
+    const cmd = 'npx sequelize-cli db:migrate';
+    const p = exec(cmd, { maxBuffer: 1024 * 1024 * 10 }, (err, stdout, stderr) => {
+      if (err) {
+        console.error('Migrations failed:', stderr || err.message);
+        return reject(err);
+      }
+      console.log('Migrations finished');
+      resolve();
+    });
+    if (p.stdout) p.stdout.pipe(process.stdout);
+    if (p.stderr) p.stderr.pipe(process.stderr);
+  });
+}
+
+runMigrations()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to run migrations, exiting.');
+    process.exit(1);
+  });
